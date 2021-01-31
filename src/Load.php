df7c71520abd04587993922a4491f582f04df7ea
blindy <ayoblindstone@gmail.com>
2021-01-31 19:58:32 +0300

    add $contents

--- src/Load.php
+++ src/Load.php
@@ -5,6 +5,7 @@ function loadRemoteUserData(string $username): string
     $httpClient = new \GuzzleHttp\Client();
     $fullUrl = "https://www.instagram.com/$username?__a=1";
     $response = $httpClient->get($fullUrl);
+    $contents = $response->getBody()->getContents();
     var_dump($response->getBody()->getContents());
 
     return "";
