67bd3082f91fe009f5981b7b3a9983c995d01fa0
blindy <ayoblindstone@gmail.com>
2021-01-31 19:57:41 +0300

    add error message

--- bin/load.php
+++ bin/load.php
@@ -5,4 +5,10 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 $username = $argv[1];
 
+if (empty($argv[1])) {
+    echo "Empty username argument. \n";
+    echo "Usage load.php <username>\n";
+    exit(1);
+}
+
 loadRemoteUserData($username);
