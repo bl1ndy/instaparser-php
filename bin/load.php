29a94170339228510d8448f73b7cabeda1367d9d
bl1ndy <ayoblindstone@gmail.com>
2021-02-02 16:45:19 +0300

    add first part of parser

--- bin/load.php
+++ bin/load.php
@@ -6,11 +6,15 @@ use function Instaparser\Load\loadRemoteUserData;
 require_once __DIR__ . "/../vendor/autoload.php";
 
 if (empty($argv[1])) {
-    echo "Empty username argument. \n";
-    echo "Usage load.php <username>\n";
+    echo "Empty username argument.\n";
+    echo "Usage: load <username> [<sessionId>]\n";
     exit(1);
 }
 
 $username = $argv[1];
-
-loadRemoteUserData($username);
+$sessionId = $argv[2] ?? null;
+if ($sessionId !== null) {
+    echo "Using sessionid = $sessionId\n";
+}
+$fullPath = loadRemoteUserData($username, $sessionId);
+echo "Data has been stored to '$fullPath'\n";
--- src/Load.php
+++ src/Load.php
@@ -2,14 +2,25 @@
 
 namespace Instaparser\Load;
 
-function loadRemoteUserData(string $username): string
+use function Instaparser\Storage\saveUserData;
+
+function loadRemoteUserData(string $username, ?string $sessionId): string
 {
     $httpClient = new \GuzzleHttp\Client();
-    $fullUrl = "https://www.instagram.com/$username?__a=1";
-    $response = $httpClient->get($fullUrl);
+    $fullUrl = "https://www.instagram.com/$username/?__a=1";
+    $options = [];
+    if ($sessionId !== null) {
+        $options = [
+            'headers' => [
+                'cookie' => "sessionid=$sessionId",
+            ],
+        ];
+    }
+    $response = $httpClient->get($fullUrl, $options);
     $contents = $response->getBody()->getContents();
     $data = json_decode($contents);
-    saveUserData($data);
-
-    return "";
+    if (!is_object($data)) {
+        throw new \Exception("User data is not object: " . $contents);
+    }
+    return saveUserData($username, $data);
 }
