a259c9ef9ef33ece96b7eace6c9e52762f698130
bl1ndy <ayoblindstone@gmail.com>
2021-02-10 14:53:14 +0300

    add the second part of parser with stat function

---
+++ src/Stat.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Instaparser\Stat;
+
+use Illuminate\Support\Collection;
+use function Instaparser\Storage\getUserData;
+
+function getUserStat(string $username): array
+{
+    $data = getUserData($username);
+    if ($data === null) {
+        throw new \Exception("Data not found for $username");
+    }
+
+    return getStatFromUserData($data);
+}
+
+function getStatFromUserData(\stdClass $data): array
+{
+    $stat = [];
+    $followers = $data->graphql->user->edge_followed_by->count ?? null;
+    $stat['followers'] = $followers;
+    $stat['posts'] = [];
+
+    $postNodes = $data
+        ->graphql
+        ->user
+        ->edge_owner_to_timeline_media
+        ->edges;
+
+    foreach ($postNodes as $nodeData) {
+        $node = $nodeData->node;
+        $comments = $node->edge_media_to_comment->count;
+        $likes = $node->edge_liked_by->count;
+        $engagements = $comments + $likes;
+        $er = null;
+        if ($followers > 0) {
+            $er = round($engagements / $followers * 100, 2);
+        }
+
+        $post = [
+            'comments' => $comments,
+            'likes' => $likes,
+            'engagements' => $engagements,
+            'er' => $er,
+            'url' => "https://www.instagram.com/p/{$node->shortcode}",
+        ];
+        $stat['posts'][] = $post;
+    }
+
+    if ($followers > 0) {
+        $stat['avgEr'] = getAverageEngagementRate($stat['posts'], $followers);
+    }
+
+    $stat['mostLikedPost'] = findTopPost($stat['posts'], 'likes');
+    $stat['mostCommentedPost'] = findTopPost($stat['posts'], 'comments');
+    $stat['topEr'] = findTopPost($stat['posts'], 'er');
+
+    return $stat;
+}
+
+function getAverageEngagementRate(array $posts, int $followers): float
+{
+    $totalEngs = 0;
+    foreach ($posts as $post) {
+        $totalEngs += $post['engagements'];
+    }
+    return round($totalEngs / $followers / count($posts) * 100, 2);
+}
+
+function findTopPost(array $posts, $property): ?array
+{
+    $collection = new Collection($posts);
+
+    return $collection->sortByDesc($property)
+        ->first();
+}
